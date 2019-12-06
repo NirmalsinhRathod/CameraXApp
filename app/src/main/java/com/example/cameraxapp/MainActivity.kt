2d9dbfb7f045e7a6e06a6b6a7e2c64945b9726e5
Neha Chauhan <neha@creolestudios.com>
2019-12-06 18:56:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/cameraxapp/MainActivity.kt
@@ -0,0 +1,203 @@
+package com.example.cameraxapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.*
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.Executors
+
+// This is an arbitrary number we are using to keep track of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts.
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        viewFinder = findViewById(R.id.view_finder)
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+// Add this after onCreate
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun startCamera() {
+
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+
+            // Add this before CameraX.bindToLifecycle
+
+            // Create configuration object for the image capture use case
+            val imageCaptureConfig = ImageCaptureConfig.Builder()
+                .apply {
+                    // We don't set a resolution for image capture; instead, we
+                    // select a capture mode which will infer the appropriate
+                    // resolution based on aspect ration and requested mode
+                    setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                }.build()
+
+            // Build the image capture use case and attach button click listener
+            val imageCapture = ImageCapture(imageCaptureConfig)
+            findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+                val file = File(externalMediaDirs.first(),
+                    "${System.currentTimeMillis()}.jpg")
+
+                imageCapture.takePicture(file, executor,
+                    object : ImageCapture.OnImageSavedListener {
+                        override fun onError(
+                            imageCaptureError: ImageCapture.ImageCaptureError,
+                            message: String,
+                            exc: Throwable?
+                        ) {
+                            val msg = "Photo capture failed: $message"
+                            Log.e("CameraXApp", msg, exc)
+                            viewFinder.post {
+                                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onImageSaved(file: File) {
+                            val msg = "Photo capture succeeded: ${file.absolutePath}"
+                            Log.d("CameraXApp", msg)
+                            viewFinder.post {
+                                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+            }
+
+            // Bind use cases to lifecycle
+            // If Android Studio complains about "this" being not a LifecycleOwner
+            // try rebuilding the project or updating the appcompat dependency to
+            // version 1.1.0 or higher.
+            CameraX.bindToLifecycle(this, preview)
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview)
+    }
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
